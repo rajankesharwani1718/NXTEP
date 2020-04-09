6bafd8865f67581ce958b88bd2e57dcdfc53ba42
Rajan Kesharwani <rajankesharwani1718@gmail.com>
2020-04-09 20:27:55 +0530

    Initial commit

---
+++ src/main/java/com/waterboll/NXTEP/NxtepApplication.java
@@ -0,0 +1,13 @@
+package com.waterboll.NXTEP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NxtepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NxtepApplication.class, args);
+	}
+
+}
